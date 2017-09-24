704431769c80d3fbca1f8aa8674a7514c44e857d
Venkatesh Joshi <venkatesh.joshi@live.com>
2017-09-24 23:21:31 +0530

    Blog list and blog detail code

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
-from .views import post_list
+from .views import post_list, post_detail
 #from . import views
 
 urlpatterns = [ 
         #url(r'^$', views.post_list, name='post_list'),
         url(r'^$', post_list, name='post_list'),
+        url(r'^post/(?P<pk>\d+)/$', post_detail, name='post_detail'),
 ]
