396367b9e47edc4f418bc538a26e12d4ee3841e8
Venkatesh Joshi <venkatesh.joshi@live.com>
2017-09-24 22:26:02 +0530

    More changes for listing blogs

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
-from . import views
+from .views import post_list
+#from . import views
 
 urlpatterns = [ 
-        url(r'^$', views.post_list, name='post_list'),
+        #url(r'^$', views.post_list, name='post_list'),
+        url(r'^$', post_list, name='post_list'),
 ]
